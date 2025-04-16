829852664395919c0bd43d8aa31574454d7b1285
Joshua Hoskins <jrh4dy@gmail.com>
2025-04-16 13:59:55 -0400

    Beginning to make this an actual installable package or testing.
    - code reorganization.
    - moving to table object soon.
    - fixed read_tsm (I think) to properly truncate data based on total_shape.

--- casaio/src/casaio/io/table.py
+++ casaio/src/casaio/io/table.py
@@ -1,4 +1,7 @@
+import pathlib
+
 import numpy as np
+import toolviper.utils.logger as logger
 
 from kaitaistruct import KaitaiStream
 
@@ -11,4 +14,20 @@ from casaio.tablestream.python.tsm_binary_data import TsmBinaryData
 
 class Table:
     def __init__(self):
+        self.base_name = None
+
+    def set_file(self, base_name: str)->None:
+
+        # Make sure the file exists
+        if pathlib.Path(base_name).exists():
+            if self.base_name is not None:
+                logger.warning(f"File name changed: {self.base_name} --> {base_name}")
+
+            self.base_name = base_name
+
+        else:
+            logger.error(f"File does not exist: {base_name}")
+
+
+    def get_column(self, name: str, is_regular_table=True)->None:
         pass
