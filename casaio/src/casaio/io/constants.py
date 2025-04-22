b3b46bc85fa6d71537be9c62352e68c59a3e779d
Joshua Hoskins <jrh4dy@gmail.com>
2025-04-22 13:03:05 -0400

    Beginning to make this an actual installable package or testing.
    - Add some data types to the constants module. Should probably change from numpy to, for instance, "<f8" to get endianess...

--- casaio/src/casaio/io/constants.py
+++ casaio/src/casaio/io/constants.py
@@ -1,5 +1,6 @@
 # Map dictionary of c++ based manager name to katitai manager class
 import numpy as np
+from numba import np_float32
 
 manager_list = {
         "StandardStMan": {
@@ -50,5 +51,35 @@ manager_list = {
 
 casacore_data_types = {
     0: np.bool,
-    9: np.complex64
+    1: np.char,
+###   2           TpUChar
+    3: np.short,
+    4: np.ushort,
+    5: np.int32,
+    6: np.uint32,
+    7: np.float32,
+    8: np.float64,
+    9: np.complex64,
+    10: np.complex128,
+    11: str,
+###   12          TpTable
+###   13          TpArrayBool
+###   14          TpArrayChar
+###   15          TpArrayUChar
+###   16          TpArrayShort
+###   17          TpArrayUShort
+###   18          TpArrayInt
+###   19          TpArrayUInt
+###   20          TpArrayFloat
+###   21          TpArrayDouble
+###   22          TpArrayComplex
+###   23          TpArrayDComplex
+###   24          TpArrayString
+###   25          TpRecord
+###   26          TpOther
+###   27          TpQuantity
+###   28          TpArrayQuantity
+    29: np.int64
+###   30          TpArrayInt64
+
 }
